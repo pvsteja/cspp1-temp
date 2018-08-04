e6b7555a550390913110ceab11651aa43a7ed942
pvsteja <tejapathri6@msitprogram.net>
2018-08-04 12:38:06 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- M6/p1/p1/fizz_buzz.py
+++ M6/p1/p1/fizz_buzz.py
@@ -12,8 +12,8 @@ def main():
     i = 1
     while i <= num_a:
         if (i%3 == 0) and (i%5 == 0):
-            print("fizz")
             print("buzz")
+            print("fizz")
         elif i%3 == 0:
             print("fizz")
         elif i%5 == 0:
